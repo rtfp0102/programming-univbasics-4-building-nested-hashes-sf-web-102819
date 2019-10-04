0372610f8f05814d9e8f4cc109b818e930f6b190
rtfp0102 <rtfp0102@gmail.com>
2019-10-04 04:26:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
         },
       :names =>
         {
-        :reading_railraod => {},
+        :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
